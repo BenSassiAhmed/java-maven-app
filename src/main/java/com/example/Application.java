c5e926a22a38c4e1f3018163494c352bb68f92ee
BenSassiAhmed <52077564+BenSassiAhmed@users.noreply.github.com>
2024-11-17 18:44:43 +0100

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,29 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Application.class, args);
     }
 
     @PostConstruct
-    public void init() {
+    public void init()
+    {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
     }
-}
 
-@RestController  // Define a REST controller
-class StatusController {
-
-    @GetMapping("/status")  // Map the /status endpoint to this method
     public String getStatus() {
         return "OK";
     }
